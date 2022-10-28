7c64678ffccf8376cea3a09bfddb7ecd58e7268b
Thông Thái <46261937+nikethai@users.noreply.github.com>
2022-10-28 23:30:50 +0700

    upd cred

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -1,7 +1,13 @@
 import { BetaAnalyticsDataClient } from "@google-analytics/data";
 import { IMostViewData } from "../interface";
 
-const analyticsDataClient = new BetaAnalyticsDataClient();
+const analyticsDataClient = new BetaAnalyticsDataClient({
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+  }
+});
 const getReportByPageViews = async (): Promise<IMostViewData[]> => {
   const [resp] = await analyticsDataClient.runReport({
     property: "properties/338030743",
